a670001f9ea0c71c3ef7020cca83d9c3ab1b6713
Roman Tudvasev <roman.tudvasev@endava.com>
2017-05-25 17:54:20 +0300

    Added basics of Elixir app

---
+++ lib/expert_api.ex
@@ -0,0 +1,18 @@
+defmodule ExpertApi do
+  @moduledoc """
+  Documentation for ExpertApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExpertApi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/expert_api_test.exs
@@ -0,0 +1,8 @@
+defmodule ExpertApiTest do
+  use ExUnit.Case
+  doctest ExpertApi
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
