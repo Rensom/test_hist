29de8419acaafc767361f693b28241ea04f7a43c
Roman Tudvasev <roman.tudvasev@endava.com>
2017-05-26 13:18:41 +0300

    Added configurations of endpoint and repo

--- lib/expert_api.ex
+++ lib/expert_api.ex
@@ -1,4 +1,5 @@
 defmodule ExpertApi do
+  use Application
   @moduledoc """
   Documentation for ExpertApi.
   """
@@ -12,7 +13,29 @@ defmodule ExpertApi do
       :world
 
   """
-  def hello do
-    :world
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Start the Ecto repository
+      supervisor(ExpertApi.Repo, []),
+      # Start the endpoint when the application starts
+      supervisor(ExpertApi.Endpoint, []),
+      # Start your own worker by calling: ApiExample.Worker.start_link(arg1, arg2, arg3)
+      # worker(ApiExample.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ExpertApi.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    ExpertApi.Endpoint.config_change(changed, removed)
+    :ok
   end
 end
---
+++ lib/expert_api/repo.ex
@@ -0,0 +1,3 @@
+defmodule ExpertApi.Repo do
+  use Ecto.Repo, otp_app: :expert_api
+end
