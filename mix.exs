dd5eb343301663a002b200309796da0119a49f51
Roman Tudvasev <roman.tudvasev@endava.com>
2017-05-26 11:02:27 +0300

    Added Logger dependency and first logger configuration

--- mix.exs
+++ mix.exs
@@ -3,18 +3,14 @@ defmodule ExpertApi.Mixfile do
 
   def project do
     [app: :expert_api,
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
@@ -28,6 +24,8 @@ defmodule ExpertApi.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:logger_file_backend, "~> 0.0.9"}
+    ]
   end
 end
