49b2df05e17ed1fea7376beca265f97bdcbc2860
Roman Tudvasev <roman.tudvasev@endava.com>
2017-05-26 13:04:35 +0300

    Added dependecies for API and some configs

--- mix.exs
+++ mix.exs
@@ -5,21 +5,31 @@ defmodule ExpertApi.Mixfile do
     [app: :expert_api,
      version: "0.0.1",
      elixir: "~> 1.4",
+     elixirc_paths: elixirc_paths(Mix.env),
+     compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
   def application do
-    [extra_applications: [:logger],
-    applications: [:mongodb, :poolboy]]
+    [
+      mod:  {ExperApi, []},
+      extra_applications: [:logger],
+      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, 
+                      :gettext, :cowboy, :mongodb, :poolboy]]
   end
 
   defp deps do
     [
+      {:phoenix, "~> 1.2.4"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
       {:logger_file_backend, "~> 0.0.9"},
       {:mongodb, ">= 0.0.0"},
-      {:poolboy, ">= 0.0.0"}
-    ]
+      {:poolboy, ">= 0.0.0"}]
   end
 end
